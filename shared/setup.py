ecaee865d58a1ebd4c1f26d69409b316697d8c7c
Mounika Kakarla <kakarlamounika21@gmail.com>
2026-07-04 13:33:51 -0400

    clean: remove shared/setup.py

--- shared/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="insights_framework",
-    version="1.0.0",
-    packages=find_packages(),
-    install_requires=[],
-    description="Shared ML pipeline base class for all BU audience models",
-)
